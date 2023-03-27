403bc44867e3be675c38781f637e92082221bed0
Administrator <czy980927@163.com>
2023-03-27 23:45:07 +0800

    1.0.0

---
+++ DroolsDemo/src/main/java/com/cui/Pojo/Order.java
@@ -0,0 +1,10 @@
+package com.cui.Pojo;
+
+
+import lombok.Data;
+
+@Data
+public class Order {
+    private Double originalPrice; // 订单原始价格，即优惠前的价格
+    private Double realPrice; // 订单真实价格，即优惠后的价格
+}
